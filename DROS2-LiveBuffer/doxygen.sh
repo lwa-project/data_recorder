fc9aabbbcb6ead293e8ffefe785dea5bf2809389
Jayce Dowell <jayce.dowell@gmail.com>
2018-07-19 22:35:09 +0000

    Cleaned up the doxygen build system for DROS.

--- DROS2-LiveBuffer/doxygen.sh
+++ DROS2-LiveBuffer/doxygen.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-FILE=/home/chwolfe2/workspace-2013/DROS2-LiveBuffer/doxyfile
-if [ `hostname` == "xps-dev" ]; then
-	echo "Generating documentation..."
-	doxygen $FILE
+FILE=./doxyfile
+echo "Generating documentation..."
+doxygen $FILE
 
-	echo "Publishing documentation to webserver..."
-	rsync -e "ssh -i /home/chwolfe2/.ssh/apache_push_id_rsa" -raz /home/chwolfe2/DROS_docs/output-livebuffer/* chwolfe2@192.168.1.10:/home/chwolfe2/DROS_docs/output-livebuffer/.
+echo "Finished Documentation"
 
-	echo "Finished Documentaiton"
-else
-	echo "Bypassing Doxygen generation (wrong build host)... "
-fi
--- DROS2-Spectrometer/doxygen.sh
+++ DROS2-Spectrometer/doxygen.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-FILE=/home/chwolfe2/workspace-2013/DROS2-Spectrometer/doxyfile
-if [ `hostname` == "xps-dev" ]; then
-	echo "Generating documentation..."
-	doxygen $FILE
+FILE=./doxyfile
+echo "Generating documentation..."
+doxygen $FILE
 
-	echo "Publishing documentation to webserver..."
-	rsync -e "ssh -i /home/chwolfe2/.ssh/apache_push_id_rsa" -raz /home/chwolfe2/DROS_docs/output-spectrometer/* chwolfe2@192.168.1.10:/home/chwolfe2/DROS_docs/output-spectrometer/.
-
-	echo "Finished Documentaiton"
-else
-	echo "Bypassing Doxygen generation (wrong build host)... "
-fi
+echo "Finished Documentation"
