b1e70eeb5380d74823c8b3eea96cbae1bdc2bd75
Jayce Dowell <jayce.dowell@gmail.com>
2018-07-23 19:54:05 +0000

    Bumped the version of DROS to reflect recent changes to the code.

--- DROS2-Spectrometer/Derived/DrosVersion.h
+++ DROS2-Spectrometer/Derived/DrosVersion.h
@@ -1,6 +1,6 @@
 #ifndef DROSVERSION_H_
 #define DROSVERSION_H_
 
-#define __DROS_VERSION__ "2.4.0.0-Spectrometer-Release"
+#define __DROS_VERSION__ "2.4.1.0-Spectrometer-Release"
 
 #endif /* DROSVERSION_H_ */
