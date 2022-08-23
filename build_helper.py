881bbda83c7bec479143e646664a591ff787c5b4
jaycedowell <jayce.dowell@gmail.com>
2022-08-23 15:01:12 -0600

    Is this better?

--- build_helper.py
+++ build_helper.py
@@ -9,13 +9,28 @@ if len(drs) == 0:
     # Standard build
     os.system("make clean && make -j all")
 else:
+    # Multi-DR build
+    ## Save where we are at
+    orig_dir = os.getcwd()
+    
+    ## Iterate over DRs to build
     for dr in drs:
+        ### Name
         try:
             dr = int(dr, 10)
             dr = f"DR{dr}"
         except ValueError:
             dr = dr.upper()
             
+        ### Build directory that we copy things into
+        if not os.path.exists(f"build_{dr}"):
+            os.mkdir(f"build_{dr}")
+        os.system(f"rsync -avH --exclude build_* . build_{dr}")
+        
+        ### Move into that build directory
+        os.chdir(f"build_{dr}")
+        
+        ### Makefile update
         os.system(f"rm -rf Makefile.{dr}")
         with open("Makefile", 'r') as im:
             with open(f"Makefile.{dr}", 'w') as om:
@@ -25,12 +40,18 @@ else:
                     elif line.startswith('STORAGE_LOCATION'):
                         line = f"STORAGE_LOCATION?=/LWA_STORAGE/{dr}\n"
                     om.write(line)
+        os.unlink("Makefile")
+        os.rename(f"Makefile.{dr}", "Makefile")
                     
+        ### Build flags
         flags  = f" -DDEFAULT_CONFIG_FILE='\\\"/LWA/{dr}/config/defaults_v2.cfg\\\"'"
         flags += f" -DDEFAULT_LOG_FILE='\\\"/LWA/{dr}/runtime/runtime.log\\\"'"
         flags += f" -DDEFAULT_TUNING_FILE='\\\"/LWA/{dr}/config/netperformance.sysctl.conf\\\"'"
         flags += f" -DDEFAULT_SCRIPT_DIR='\\\"/LWA/{dr}/scripts\\\"'"
         flags += f" -DDEFAULT_STORAGE_DIR='\\\"/LWA_STORAGE/{dr}\\\"'"
         
+        ### Clean and build
         os.system("make clean")
-        os.system(f"CPPFLAGS=\"{flags}\" make -j all -f Makefile.{dr}")
+        os.system(f"CPPFLAGS=\"{flags}\" make -j all")
+        
+        os.chdir(orig_dir)
