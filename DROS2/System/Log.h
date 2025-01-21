5517adc7407e6479ba816dab4df1cb2d76717b7d
jaycedowell <jayce.dowell@gmail.com>
2025-01-21 07:07:20 -0700

    Drop boost/progress.hpp.

--- DROS2/System/Log.h
+++ DROS2/System/Log.h
@@ -49,10 +49,8 @@
 
 #ifndef LOG_H_
 #define LOG_H_
-#define BOOST_TIMER_ENABLE_DEPRECATED
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
-#include <boost/progress.hpp>
 #include "../Primitives/StringList.h"
 #include "../Threading/LockHelper.h"
 #include <unistd.h>
